ba3ff5b94573606dee56ddf2be67288f20c256af
josh1510 <joshmoon.1510@gmail.com>
2021-04-03 23:04:44 +1100

    Adding code to make calculator buttons, modified from etch a sketch project

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const container = document.getElementById('grid-container');
+
+const makeCalculatorButtons = () => {
+    container.style.setProperty('--grid-rows', 5);
+    container.style.setProperty('--grid-cols', 4);
+    for (i = 0; i < 20; i++) {
+        let CalculatorButton = document.createElement('div');
+        CalculatorButton.addEventListener('mouseup', function () {
+            //add in code to append number to sum / print opperator
+        });
+        container.appendChild(CalculatorButton).className = 'calculator-button';
+    }
+};
+
+makeCalculatorButtons();
+
 //Calculator functions
 const addition = (a, b) => a + b;
 
