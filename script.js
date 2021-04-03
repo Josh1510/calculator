8561b1c22e36e283bf2048b1c390b3f9e8b763c8
josh1510 <joshmoon.1510@gmail.com>
2021-04-04 00:17:06 +1100

    add arrays for button content, add if/else to add conent to buttons

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 const container = document.getElementById('button-container');
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
+const operators = [
+    '%',
+    'CE',
+    'C',
+    '<-',
+    '1/X',
+    'x²',
+    '2√x',
+    '÷',
+    'X',
+    '-',
+    '+',
+    '+/-',
+    '=',
+];
+
+var numbersCounter = 0;
+var operatorsCounter = 0;
 
 const makeCalculatorButtons = () => {
-    container.style.setProperty('--grid-rows', 5);
+    container.style.setProperty('--grid-rows', 6);
     container.style.setProperty('--grid-cols', 4);
-    for (i = 0; i < 20; i++) {
-        let CalculatorButton = document.createElement('div');
-        CalculatorButton.addEventListener('mouseup', function () {
+    for (i = 0; i < 24; i++) {
+        let calculatorButton = document.createElement('div');
+        calculatorButton.addEventListener('click', function () {
+            console.log(calculatorButton.innerText);
             //add in code to append number to sum / print opperator
         });
-        container.appendChild(CalculatorButton).className = 'calculator-button';
+        container.appendChild(calculatorButton).className = 'calculator-button';
+        if (i < 8 || (i + 1) % 4 == 0 || i == 20) {
+            container.appendChild(calculatorButton).className +=
+                ' operator-button';
+            content = document.createTextNode(operators[operatorsCounter]);
+            calculatorButton.appendChild(content);
+            operatorsCounter++;
+        } else {
+            container.appendChild(calculatorButton).className +=
+                ' number-button';
+            content = document.createTextNode(numbers[numbersCounter]);
+            calculatorButton.appendChild(content);
+            numbersCounter++;
+        }
     }
 };
 
 makeCalculatorButtons();
 
-//Calculator functions
+//Calculator operators / functions
 const addition = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
