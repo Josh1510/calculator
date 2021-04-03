c3d958447d12a3e48390b83f461ca777f597921a
josh1510 <joshmoon.1510@gmail.com>
2021-04-03 22:30:45 +1100

    added math functions

---
+++ script.js
@@ -0,0 +1,18 @@
+//Calculator functions
+const addition = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const devide = (a, b) => a / b;
+
+const square = (a) => a ** 2;
+
+const squareRoot = (a) => Math.sqrt(a);
+
+const reciprocal = (a) => 1 / a;
+
+const percentage = (a, b) => b / reciprocal(a) / 100;
+
+const changeArithmatic = (a) => a * -1;
