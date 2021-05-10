c8df6d567a7605bfdfd55a5f4e4cac6fa1f50a5c
josh1510 <joshmoon.1510@gmail.com>
2021-05-10 20:13:59 +1000

    removing button creation from js file and building with html file

--- script.js
+++ script.js
@@ -19,34 +19,6 @@ const operators = [
 var numbersCounter = 0;
 var operatorsCounter = 0;
 
-const makeCalculatorButtons = () => {
-    container.style.setProperty('--grid-rows', 6);
-    container.style.setProperty('--grid-cols', 4);
-    for (i = 0; i < 24; i++) {
-        let calculatorButton = document.createElement('div');
-        calculatorButton.addEventListener('click', function () {
-            console.log(calculatorButton.innerText);
-            //add in code to append number to sum / print opperator
-        });
-        container.appendChild(calculatorButton).className = 'calculator-button';
-        if (i < 8 || (i + 1) % 4 == 0 || i == 20) {
-            container.appendChild(calculatorButton).className +=
-                ' operator-button';
-            content = document.createTextNode(operators[operatorsCounter]);
-            calculatorButton.appendChild(content);
-            operatorsCounter++;
-        } else {
-            container.appendChild(calculatorButton).className +=
-                ' number-button';
-            content = document.createTextNode(numbers[numbersCounter]);
-            calculatorButton.appendChild(content);
-            numbersCounter++;
-        }
-    }
-};
-
-makeCalculatorButtons();
-
 //Calculator operators / functions
 const addition = (a, b) => a + b;
 
