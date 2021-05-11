39117141bb77e01319d6a188374b7dcf5bfc4424
josh1510 <joshmoon.1510@gmail.com>
2021-05-11 17:27:52 +1000

    setting clearAll function, stating on operator function to calculate sum

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const numberButtons = document.querySelectorAll('.number-button');
 const screen = document.querySelector('#screen');
+const screenSum = document.querySelector('#screen-sum');
+
+let operation;
+let firstValue;
+let secondValue;
+let total = '';
 
 //selectors for operator buttons
 const percentageButton = document.querySelector('#percentage');
@@ -38,6 +44,18 @@ changeArithmaticButton.addEventListener('click', changeArithmatic);
 equalButton.addEventListener('click', equal);
 
 decimalButton.addEventListener('click', decimal);
+
+//Calculator operators / functions
+// const addition = (a, b) => a + b;
+// const subtract = (a, b) => a - b;
+// multiply = (a, b) => a * b;
+// const devide = (a, b) => a / b;
+// const square = (a) => a ** 2;
+// const squareRoot = (a) => Math.sqrt(a);
+// const reciprocal = (a) => 1 / a;
+// const percentage = (a, b) => b / reciprocal(a) / 100;
+// const changeArithmatic = (a) => a * -1;
+
 /////
 numberButtons.forEach((numberButton) =>
     numberButton.addEventListener('click', () => appendValue(numberButton.id))
@@ -52,9 +70,16 @@ function percentage() {}
 //removes all entered numbers on the current line
 function clearEntry() {
     screen.textContent = '';
+    total = '';
 }
 
-function clearAll() {}
+function clearAll() {
+    firstValue = null;
+    secondValue = null;
+    screenSum.textContent = '';
+    screen.textContent = '';
+    total = '';
+}
 
 //remove the last entered number
 function backspaceNumber() {
@@ -65,19 +90,13 @@ function reciprocal() {}
 function square() {}
 function squareRoot() {}
 function devide() {}
-function multiply() {}
+function multiply() {
+    operator('x');
+}
 function subtract() {}
 function addition() {}
 function changeArithmatic() {}
 function equal() {}
-
-// function decimal() {
-//     return screen.textContent.includes('.')
-//         ? ''
-//         : screen.textContent === ''
-//         ? (screen.textContent = '0.')
-//         : (screen.textContent += '.');
-// }
 
 function decimal() {
     if (screen.textContent.includes('.')) {
@@ -90,13 +109,31 @@ function decimal() {
         : (screen.textContent += '.');
 }
 
-//Calculator operators / functions
-// const addition = (a, b) => a + b;
-// const subtract = (a, b) => a - b;
-// const multiply = (a, b) => a * b;
-// const devide = (a, b) => a / b;
-// const square = (a) => a ** 2;
-// const squareRoot = (a) => Math.sqrt(a);
-// const reciprocal = (a) => 1 / a;
-// const percentage = (a, b) => b / reciprocal(a) / 100;
-// const changeArithmatic = (a) => a * -1;
+function operator(operator) {
+    if (screen.textContent == total) {
+        screenSum.textContent = total;
+        screen.textContent = '';
+    }
+
+    firstValue === null || firstValue === undefined
+        ? (firstValue = screen.textContent)
+        : (secondValue = screen.textContent);
+
+    screenSum.textContent = screen.textContent;
+    screenSum.textContent += ' ' + operator;
+
+    if (secondValue != null || secondValue != undefined) {
+        total = calculate(firstValue, operator, secondValue);
+        screenSum.textContent =
+            firstValue + ' ' + operator + ' ' + secondValue + ' =';
+    }
+
+    screen.textContent = total;
+}
+
+function calculate(firstValue, operator, secondValue) {
+    switch (operator) {
+        case 'x':
+            return firstValue * secondValue;
+    }
+}
